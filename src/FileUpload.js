9d0b75b5c55769b29e3a899607f7bfde8eb2f0b7
Naga Satish Arigela <70377531+NagaSatishArigela@users.noreply.github.com>
2020-12-05 10:34:17 +0530

    test

---
+++ src/FileUpload.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './FileUpload.css';
+
+function FileUpload() {
+    return (
+        <div>
+            <h1>Test</h1>
+        </div>
+    )
+}
+
+export default FileUpload
